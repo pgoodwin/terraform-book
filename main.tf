af72400b0e872eb305673b9c15825a45737c06a4
Phil Goodwin <phil.goodwin@gmail.com>
2021-02-21 20:46:22 -0800

    Got webserver working

--- main.tf
+++ main.tf
@@ -5,23 +5,18 @@ provider "aws" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "none"
-    from_port = 8080
-    ipv6_cidr_blocks = [ "::/0" ]
-    prefix_list_ids = [ "" ]
-    protocol = "tcp"
-    security_groups = [ "" ]
-    self = false
-    to_port = 8080
-  } ]
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "aws_security_group.instance.id" ]
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     name = "phils_example_tag"
