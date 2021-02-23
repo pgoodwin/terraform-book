e0317955df46e498f2aad1353d8494415f82aebe
Phil Goodwin <phil.goodwin@gmail.com>
2021-02-22 21:07:53 -0800

    Loadbalancing

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
+# Preamble
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Input values
 variable "web-server-port" {
   description = "port our web server will run on"
   type        = number
   default     = 8080
+}
+
+variable "alb-ingress-port" {
+  description = "port our web server will run on"
+  type        = number
+  default     = 80
 }
 
 data "aws_vpc" "default" {
@@ -12,15 +24,13 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
-output "public_ip" {
-  description = "Public IP address of the web server"
-  value       = aws_instance.example.public_ip
+# Output values
+output "public_url" {
+  description = "URL of the web server"
+  value       = aws_lb.example.dns_name
 }
 
-provider "aws" {
-  region = "us-east-2"
-}
-
+# Instances
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -47,9 +57,12 @@ resource "aws_launch_configuration" "example" {
     EOF
 }
 
-resource "aws_auto_scaling_group" "example" {
+resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -57,6 +70,80 @@ resource "aws_auto_scaling_group" "example" {
   tag {
     key                 = "name"
     value               = "example_name"
-    propogate_at_launch = true
+    propagate_at_launch = true
+  }
+}
+
+# Load balancing
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: pthbbbbt!"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.web-server-port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
